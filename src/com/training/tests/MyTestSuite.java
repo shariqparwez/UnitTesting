5061b4c9aa73cad4413e4803fef04943536e58fd
Shariq Parwez <sparwe@sapient.com>
2016-02-10 16:26:50 +0530

    Feb 10th

---
+++ src/com/training/tests/MyTestSuite.java
@@ -0,0 +1,14 @@
+package com.training.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value={FirstTestCase.class,SecondTestCase.class})
+public class MyTestSuite {
+
+}
